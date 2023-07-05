6d42254b7365d7bc666449654c81d89cae8088b7
Saranjan06 <y.saranjan@gmail.com>
2023-07-05 22:04:14 +0200

    adding new functionalities + tests

--- src/main/java/yatzy/game/Yatzy.java
+++ src/main/java/yatzy/game/Yatzy.java
@@ -1,6 +1,7 @@
 package yatzy.game;
 
 import yatzy.models.Dice;
+import yatzy.models.DiceNumbersEnum;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -19,8 +20,97 @@ public class Yatzy {
         return sumDices(dices);
     }
 
+    /**
+     * Returns the score of YATZY_WINNING_SCORE if same occurrences were found as much as dices
+     * @param dices
+     * @return YATZY_WINNING_SCORE if yatzy, 0 else
+     */
+    public static int yatzy(List<Dice> dices) {
+        final int YATZY_WINNING_SCORE = 50;
+        return getAppearanceOfEachSide(dices).entrySet().stream().anyMatch(faceNumber -> faceNumber.getValue().intValue() == dices.size()) ? YATZY_WINNING_SCORE : 0;
+    }
+
+    /**
+     * Returns the occurances of DiceNumbersEnum.ONE
+     * @param dices
+     * @return
+     */
+    public static int ones(List<Dice> dices) {
+        return getAppearanceOf(dices, DiceNumbersEnum.ONE);
+    }
+
+    /**
+     * Returns the score for the occurances of DiceNumbersEnum.TWO
+     * @param dices
+     * @return (DiceNumbersEnum.TWO * the occurances of DiceNumbersEnum.TWO)
+     */
+    public static int twos(List<Dice> dices) {
+        return DiceNumbersEnum.TWO.getNumber() * (getAppearanceOf(dices, DiceNumbersEnum.TWO));
+    }
+
+    /**
+     * Returns the score for the occurances of DiceNumbersEnum.THREE
+     * @param dices
+     * @return (DiceNumbersEnum.THREE * the occurances of DiceNumbersEnum.THREE)
+     */
+    public static int threes(List<Dice> dices) {
+        return DiceNumbersEnum.THREE.getNumber() * (getAppearanceOf(dices, DiceNumbersEnum.THREE));
+    }
+
+
+    /**
+     * Returns the score for the occurances of DiceNumbersEnum.FOUR
+     * @param dices
+     * @return (DiceNumbersEnum.FOUR * the occurances of DiceNumbersEnum.FOUR)
+     */
+    public static int fours(List<Dice> dices) {
+        return DiceNumbersEnum.FOUR.getNumber() * (getAppearanceOf(dices, DiceNumbersEnum.FOUR));
+    }
+
+    /**
+     * Returns the score for the occurances of DiceNumbersEnum.FIVE
+     * @param dices
+     * @return (DiceNumbersEnum.FIVE * the occurances of DiceNumbersEnum.FIVE)
+     */
+    public static int fives(List<Dice> dices) {
+        return DiceNumbersEnum.FIVE.getNumber() * (getAppearanceOf(dices, DiceNumbersEnum.FIVE));
+    }
+
+
+    /**
+     * Returns the score for the occurances of DiceNumbersEnum.SIX
+     * @param dices
+     * @return (DiceNumbersEnum.SIX * the occurances of DiceNumbersEnum.SIX)
+     */
+    public static int sixes(List<Dice> dices) {
+        return DiceNumbersEnum.SIX.getNumber() * (getAppearanceOf(dices, DiceNumbersEnum.SIX));
+    }
 
 /** *************************************************************************************************************** **/
+
+    /**
+     * Returns the number of occurances of a side
+     * @param dices
+     * @param numberEnum
+     * @return
+     */
+    private static int getAppearanceOf(List<Dice> dices, DiceNumbersEnum numberEnum) {
+        Optional<Map.Entry<Integer, Integer>> result =  getAppearanceOfEachSide(dices).entrySet().stream().filter(entry -> numberEnum.getNumber() == entry.getKey().intValue()).findFirst();
+        if (result.isPresent()) {
+            return result.get().getValue();
+        }
+        return 0;
+    }
+
+    /**
+     * Return a map of each number with its occurances
+     * @param dices
+     * @return
+     */
+    private static LinkedHashMap<Integer, Integer> getAppearanceOfEachSide(List<Dice> dices) {
+        return dices.stream().collect(Collectors.groupingBy(d -> d.getSelectedSide() != null ? d.getSelectedSide().getNumber() : 0, LinkedHashMap::new,
+            Collectors.summingInt(a -> 1)));
+    }
 
     /**
      * Returns the sum of the selected side of the dice
@@ -30,4 +120,6 @@ public class Yatzy {
     private static int sumDices(List<Dice> dices) {
         return dices.stream().collect(Collectors.summingInt(e -> e.getSelectedSide().getNumber()));
     }
+
+
 }
--- src/test/java/yatzy/game/TestYatzy.java
+++ src/test/java/yatzy/game/TestYatzy.java
@@ -1,11 +1,8 @@
 package yatzy.game;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import yatzy.models.Dice;
-import yatzy.models.DiceNumbersEnum;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -14,16 +11,191 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 /**
  * @author saran
  */
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class TestYatzy {
 
     @Test
     @DisplayName("Testing chance()...")
+    @Order(1)
     public void testChance() {
+        //first test
         int[] selectedNumbers = new int [] {2, 3, 4, 5, 1};
         List<Dice> dices = GameFactory.initDices(selectedNumbers);
         int output = Yatzy.chance(dices);
         int expected = 15;
-        assertEquals(expected, output, new StringBuilder().append(Arrays.toString(selectedNumbers)).append(" should give ").append(expected).toString());
+        assertEquals(expected, output, errorMessage(selectedNumbers, expected));
+
+        //second test
+        selectedNumbers = new int[] {3, 3, 4, 5, 1};
+        dices = GameFactory.initDices(selectedNumbers);
+        output = Yatzy.chance(dices);
+        expected = 16;
+        assertEquals(expected, output, errorMessage(selectedNumbers, expected));
+    }
+
+    @Test
+    @DisplayName("Testing yatzy()...")
+    @Order(2)
+    public void  testYatzy() {
+        //first test
+        int[] selectedNumbers = {4, 4, 4, 4, 4};
+        int output = Yatzy.yatzy(GameFactory.initDices(selectedNumbers));
+        int expected = 50;
+        assertEquals(expected, output, errorMessage(selectedNumbers, expected));
+
+        //second test
+        selectedNumbers = new int[] {6, 6, 6, 6, 6};
+        output = Yatzy.yatzy(GameFactory.initDices(selectedNumbers));
+        expected = 50;
+        assertEquals(expected, output, errorMessage(selectedNumbers, expected));
+
+        //third test
+        selectedNumbers = new int[] {6, 6, 6, 6, 3};
+        expected = 0;
+        output = Yatzy.yatzy(GameFactory.initDices(selectedNumbers));
+        assertEquals(expected, output, errorMessage(selectedNumbers, expected));
+    }
+
+    @Test
+    @DisplayName("Testing ones()...")
+    @Order(3)
+    public void testOnes() {
+        //first test
+        int[] selectedNumbers = {1, 2, 3, 4, 5};
+        int output = Yatzy.ones(GameFactory.initDices(selectedNumbers));
+        int expected = 1;
+        assertEquals(expected, output, errorMessage(selectedNumbers, expected));
+
+        //second test
+        selectedNumbers = new int[]{1, 2, 1, 4, 5};
+        output = Yatzy.ones(GameFactory.initDices(selectedNumbers));
+        expected = 2;
+        assertEquals(expected, output, errorMessage(selectedNumbers, expected));
+
+        //third test
+        selectedNumbers = new int[]{6, 2, 2, 4, 5};
+        output = Yatzy.ones(GameFactory.initDices(selectedNumbers));
+        expected = 0;
+        assertEquals(expected, output, errorMessage(selectedNumbers, expected));
+
+        //forth test
+        selectedNumbers = new int[]{1, 2, 1, 1, 1};
+        output = Yatzy.ones(GameFactory.initDices(selectedNumbers));
+        expected = 4;
+        assertEquals(expected, output, errorMessage(selectedNumbers, expected));
+    }
+
+    @Test
+    @DisplayName("Testing twos()...")
+    @Order(4)
+    public void testTwos() {
+        //first test
+        int[] selectedNumbers = {1, 2, 3, 2, 6};
+        int output = Yatzy.twos(GameFactory.initDices(selectedNumbers));
+        int expected = 4;
+        assertEquals(expected, output, errorMessage(selectedNumbers, expected));
+
+        //second test
+        selectedNumbers = new int[]{2, 2, 2, 2, 2};
+        output = Yatzy.twos(GameFactory.initDices(selectedNumbers));
+        expected = 10;
+        assertEquals(expected, output, errorMessage(selectedNumbers, expected));
+    }
+
+    @Test
+    @DisplayName("Testing threes()...")
+    @Order(5)
+    public void testThrees() {
+        //first test
+        int[] selectedNumbers = {1, 2, 3, 2, 3};
+        int output = Yatzy.threes(GameFactory.initDices(selectedNumbers));
+        int expected = 6;
+        assertEquals(expected, output, errorMessage(selectedNumbers, expected));
+
+        //second test
+        selectedNumbers = new int[]{2, 3, 3, 3, 3};
+        output = Yatzy.threes(GameFactory.initDices(selectedNumbers));
+        expected = 12;
+        assertEquals(expected, output, errorMessage(selectedNumbers, expected));
+    }
+
+    @Test
+    @DisplayName("Testing fours()...")
+    @Order(6)
+    public void testFours() {
+        //first test
+        int[] selectedNumbers = {4, 4, 4, 5, 5};
+        int output = Yatzy.fours(GameFactory.initDices(selectedNumbers));
+        int expected = 12;
+        assertEquals(expected, output, errorMessage(selectedNumbers, expected));
+
+        //second test
+        selectedNumbers = new int[]{4, 4, 5, 5, 5};
+        output = Yatzy.fours(GameFactory.initDices(selectedNumbers));
+        expected = 8;
+        assertEquals(expected, output, errorMessage(selectedNumbers, expected));
+
+        //third test
+        selectedNumbers = new int[]{4, 5, 5, 5, 5};
+        output = Yatzy.fours(GameFactory.initDices(selectedNumbers));
+        expected = 4;
+        assertEquals(expected, output, errorMessage(selectedNumbers, expected));
+    }
+
+    @Test
+    @DisplayName("Testing fives()...")
+    @Order(7)
+    public void testFives() {
+        //first test
+        int[] selectedNumbers = {4, 4, 4, 5, 5};
+        int output = Yatzy.fives(GameFactory.initDices(selectedNumbers));
+        int expected = 10;
+        assertEquals(expected, output, errorMessage(selectedNumbers, expected));
+
+        //second test
+        selectedNumbers = new int[]{4, 4, 5, 5, 5};
+        output = Yatzy.fives(GameFactory.initDices(selectedNumbers));
+        expected = 15;
+        assertEquals(expected, output, errorMessage(selectedNumbers, expected));
+
+        //third test
+        selectedNumbers = new int[]{4, 5, 5, 5, 5};
+        output = Yatzy.fives(GameFactory.initDices(selectedNumbers));
+        expected = 20;
+        assertEquals(expected, output, errorMessage(selectedNumbers, expected));
+    }
+
+    @Test
+    @DisplayName("Testing sixes()...")
+    @Order(8)
+    public void testSixes() {
+        //first test
+        int[] selectedNumbers = {4, 4, 4, 5, 5};
+        int output = Yatzy.sixes(GameFactory.initDices(selectedNumbers));
+        int expected = 0;
+        assertEquals(expected, output, errorMessage(selectedNumbers, expected));
+
+        //second test
+        selectedNumbers = new int[]{4, 4, 6, 5, 5};
+        output = Yatzy.sixes(GameFactory.initDices(selectedNumbers));
+        expected = 6;
+        assertEquals(expected, output, errorMessage(selectedNumbers, expected));
+
+        //third test
+        selectedNumbers = new int[]{6, 5, 6, 6, 5};
+        output = Yatzy.sixes(GameFactory.initDices(selectedNumbers));
+        expected = 18;
+        assertEquals(expected, output, errorMessage(selectedNumbers, expected));
+    }
+
+    /**
+     * Returns the formatted message
+     * @param input : number of dices
+     * @param expectedOutPut : the score we are expecting
+     * @return
+     */
+    private static String errorMessage(int[] input, int expectedOutPut) {
+        return new StringBuilder().append(Arrays.toString(input)).append(" should give ").append(expectedOutPut).toString();
     }
 
 }
